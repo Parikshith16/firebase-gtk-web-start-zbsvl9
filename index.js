d7bf3866422a04c5c7ed50eb344da47b018482c0
Parikshith16 <75253523+Parikshith16@users.noreply.github.com>
2023-07-16 10:32:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {} from 'firebase/auth';
+import {} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  // Add Firebase project configuration object here
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-jO2hUKqAOVOTSePOh2juUOCpngn2AXI",
+    authDomain: "fir-web-codelab-867d6.firebaseapp.com",
+    projectId: "fir-web-codelab-867d6",
+    storageBucket: "fir-web-codelab-867d6.appspot.com",
+    messagingSenderId: "58513686965",
+    appId: "1:58513686965:web:e602719393ad7332dc99fc"
+  };
+
+  // initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(auth);
+}
+main();
